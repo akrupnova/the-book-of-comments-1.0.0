6f386e34003cf3235369f9ea81f215b2e86be403
akrupnova <anna.krupnovaa@gmail.com>
2024-08-08 17:45:40 -0700

    first test request

---
+++ src/_test/user/userCreate.spec.js
@@ -0,0 +1,43 @@
+const {expect} = require('chai');
+const {userCreateQ} = require('./queries');
+const {user} = require('./data');
+const gqlRequest = require('../gqlRequest');
+
+
+let respData = null;
+let postData = null;
+
+describe('USER CREATE', () => {
+    describe('USER CREATE - POSITIVE TESTS', () => {
+        it('user create all fields', () => {
+            postData = {
+                query: userCreateQ,
+                variable: user
+            }
+
+            gqlRequest(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err)
+                    respData = res.body;
+                    console.log(respData);
+                    //expect(respData).eq();
+                    done()
+                })
+        })
+
+        it('', () => {
+
+        })
+
+
+
+    })
+
+    describe('USER CREATE - NEGATIVE TESTS', () => {
+        it('', () => {
+
+        })
+
+    })
+})
